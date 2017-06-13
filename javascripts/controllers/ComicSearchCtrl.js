c79f6de127c7aba6049c91b6b801fa2cb9a126fe
jmarcusgill <jmarcusgill@gmail.com>
2017-06-12 21:16:33 -0500

    can make comic search on api, working on ironing it out. leaving for the night

--- javascripts/controllers/ComicSearchCtrl.js
+++ javascripts/controllers/ComicSearchCtrl.js
@@ -1,4 +1,18 @@
-app.controller("ComicSearchCtrl", function(){
+app.controller("ComicSearchCtrl", function($scope, $rootScope, ComicFactory){
+
+  $scope.newComic = { };
+  $scope.comicBookArray = [];
+
+  $scope.findComics = ( ) => {
+    // $scope.newComic.uid = $rootScope.user.uid;
+      console.log("in ctrl $scope.newComic:",$scope.newComic);
+    ComicFactory.findAllComics($scope.newComic).then((comicBookArray) => {
+      $scope.newComic = { };
+    }).catch((error) => {
+      console.log("error", error);
+    });
+  };
+
 console.log("inside ComicSearchCtrl");
 
 
--- javascripts/factories/ComicFactory.js
+++ javascripts/factories/ComicFactory.js
@@ -1,3 +1,26 @@
-app.factory("ComicFactory", function($http, $q, FIREBASE_CONFIG) {
+app.factory("ComicFactory", function($http, $q,  COMICVINE_CONFIG) {
+
+  //referenced in ComicSearchCtrl.js, getting newComic from api call
+  let findAllComics = (newComic) => {
+    let comicBookArray = [];
+    return $q ((resolve, reject) => {
+     $http.get(`http://comicvine.gamespot.com/api/volumes/?api_key=${COMICVINE_CONFIG.apiKey}&format=json&filter=name:${newComic}`, JSON.stringify(newComic))
+      .then((resultz) => {
+        let comicCollection = resultz.data;
+        if (comicCollection !== null) {
+            Object.keys(comicCollection).forEach((key) => {
+            comicCollection[key].id=key;
+            comicBookArray.push(comicCollection[key]);
+          });
+        }
+        console.log("in comicFactory", comicBookArray);
+        resolve(comicBookArray);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+  return {findAllComics:findAllComics};
 
 });
